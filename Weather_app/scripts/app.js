acaeb7a67d0a7fa99133e40203839ba26f2daef0
samcesa45 <47789778+samcesa45@users.noreply.github.com>
2020-06-19 10:21:39 +0100

    Add files via upload

---
+++ Weather_app/scripts/app.js
@@ -0,0 +1,81 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  //   const cityDets = data.cityDets;
+  //   const weather = data.weather;
+  console.log(data);
+
+  //destructure properties
+  const { cityDets, weather } = data;
+
+  //update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+      
+    </div>
+  `;
+
+  //Update night&day and icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  //Ternary Operator
+  const timeSrc = weather.isDayTime ? "img/day.svg" : "img/night.svg";
+  // let timeSrc = null;
+  // if (weather.isDayTime) {
+  //   timeSrc = "img/day.svg";
+  // } else {
+  //   timeSrc = "img/night.svg";
+  // }
+  time.setAttribute("src", timeSrc);
+  //remove the none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return {
+    cityDets: cityDets,
+    weather: weather,
+  };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  //prevent city default
+  e.preventDefault();
+
+  //get city
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //update the ui with new city
+  updateCity(city)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  //set the local storage
+
+  localStorage.setItem("city", city);
+});
+
+if (localStorage.getItem("city")) {
+  updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
---
+++ Weather_app/scripts/forecast.js
@@ -0,0 +1,30 @@
+const key = "wzzmhCaU8rbfBv1IQvc3DlhOLVBOl93U";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//Get City information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+getCity("manchester")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
